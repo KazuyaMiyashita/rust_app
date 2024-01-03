fbd9c535286d4988c2a3b92dd475159a9b0240e7
KazuyaMiayshita <miy1924@gmail.com>
2024-01-03 17:58:36 +0900

    なんちゃってタイマー

--- x86_64/src/main.rs
+++ x86_64/src/main.rs
@@ -6,10 +6,12 @@ use core::str;
 use core::fmt::Write;
 use pc_keyboard::PCKeyboard;
 use vga_buffer::Writer;
+use timer::Timer;
 use app::{App, Key};
 
 mod vga_buffer;
 mod pc_keyboard;
+mod timer;
 
 #[no_mangle] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
@@ -20,6 +22,7 @@ pub extern "C" fn _start() -> ! {
 
     let mut keyboard = PCKeyboard::init();
     let mut writer = Writer::init();
+    let timer = Timer::init();
 
     writeln!(writer, "Hello Waku-waku mi12cp World {}", 42).unwrap();
     writeln!(writer, "").unwrap();
@@ -31,16 +34,18 @@ pub extern "C" fn _start() -> ! {
     loop {
         let c = keyboard.get_char_blocking();
 
-        let app_elapsed_time: u128 = 0;
+        let app_elapsed_time = timer.get_elapsed_time_ms();
+        writeln!(writer, "time: {}", app_elapsed_time).unwrap();
+
         match c {
-            'a' => app.on_key_down(Key::AllowLeft, app_elapsed_time),
-            's' => app.on_key_down(Key::AllowDown, app_elapsed_time),
-            'd' => app.on_key_down(Key::AllowRight, app_elapsed_time),
-            'w' => app.on_key_down(Key::AllowUp, app_elapsed_time),
+            'a' => app.on_key_down(Key::AllowLeft, app_elapsed_time as u128),
+            's' => app.on_key_down(Key::AllowDown, app_elapsed_time as u128),
+            'd' => app.on_key_down(Key::AllowRight, app_elapsed_time as u128),
+            'w' => app.on_key_down(Key::AllowUp, app_elapsed_time as u128),
             _ => ()
         }
 
-        let total_durations = app.format_total_durations_long(app_elapsed_time);
+        let total_durations = app.format_total_durations_long(app_elapsed_time as u128);
 
         writer.clear_all();
         writeln!(writer, "{}\n{}\n{}\n{}",
