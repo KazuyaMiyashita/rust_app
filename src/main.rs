c5da1e477c51c62dfd69e8bc2ce5b034f77fb263
KazuyaMiayshita <miy1924@gmail.com>
2024-01-03 12:03:31 +0900

    矢印キー入力、入力をターミナルに出さない、出力のたびにターミナルをクリア

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,57 @@
-use std::{io, process};
+extern crate termios;
+
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
 use app::{App, Key};
 
 fn main() {
-
-    println!("Hello, world!
-カウンターが4つもついたすごいアプリだよ
-
-矢印キーのように a, w, s, d のどれかを入力してね
-q を入力すると終了するよ
-");
-
     let mut app = App::init();
 
+    let stdin = 0; // couldn't get std::os::unix::io::FromRawFd to work
+    // on /dev/stdin or /dev/tty
+    let termios = Termios::from_fd(stdin).unwrap();
+    let mut new_termios = termios.clone();  // make a mutable copy of termios
+    // that we will modify
+    new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
+    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+
+    let stdout = io::stdout();
+    let mut reader = io::stdin();
+
+    println!("press arrow keys or press 'q' to exit");
+    stdout.lock().flush().unwrap();
+
     loop {
-        println!("{}", app_to_string(&app));
+        let mut buf = [0u8; 3];
+        reader.read_exact(&mut buf[..1]).unwrap();
 
-        let mut line = String::new();
-        io::stdin().read_line(&mut line).unwrap();
+        if buf[0] == b'q' { break; }
 
-        match line.trim() {
-            "a" => app.on_key_down(Key::AllowLeft),
-            "s" => app.on_key_down(Key::AllowDown),
-            "d" => app.on_key_down(Key::AllowRight),
-            "w" => app.on_key_down(Key::AllowUp),
-            "q" => process::exit(0),
-            other => println!("unrecognized key: {}", other)
+        // Check if it's an escape sequence
+        if buf[0] == 27 {
+            // Read two more bytes to complete the escape sequence
+            reader.read_exact(&mut buf[1..]).unwrap();
+
+            // Process the escape sequence for arrow keys
+            match &buf[..] {
+                [27, 91, 65] => app.on_key_down(Key::AllowUp),
+                [27, 91, 66] => app.on_key_down(Key::AllowDown),
+                [27, 91, 67] => app.on_key_down(Key::AllowRight),
+                [27, 91, 68] => app.on_key_down(Key::AllowLeft),
+                _ => (),
+            }
         }
+
+        // Clear the terminal using escape sequence
+        print!("\x1B[2J\x1B[H"); // ESC[2JESC[H
+        println!("{}", app_to_string(&app));
     }
 
+    println!("finish");
+    tcsetattr(stdin, TCSANOW, &termios).unwrap();  // reset the stdin to
+    // original termios data
 }
 
 fn app_to_string(app: &App) -> String {
