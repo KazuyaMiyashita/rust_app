15c17fc5ed40a75c667aff52a1424907312e8552
KazuyaMiayshita <miy1924@gmail.com>
2024-01-21 17:08:16 +0900

    wip 4種の押されたボタンをキューにつめる

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use panic_probe as _;
 use rp_pico as bsp;
 
 use embedded_hal::blocking::delay::DelayMs;
-use embedded_hal::digital::v2::{InputPin, ToggleableOutputPin};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 
 use bsp::hal::fugit::RateExtU32;
 use bsp::hal::{
@@ -44,27 +44,45 @@ extern crate alloc;
 // We'll create some type aliases using `type` to help with that
 
 /// This pin will be our output - it will drive an LED if you run this on a Pico
-type LedPin = gpio::Pin<gpio::bank0::Gpio25, gpio::FunctionSioOutput, gpio::PullNone>;
+// type LedPin = gpio::Pin<gpio::bank0::Gpio25, gpio::FunctionSioOutput, gpio::PullNone>;
 
 /// This pin will be our interrupt source.
 /// ~~It will trigger an interrupt if pulled to ground (via a switch or jumper wire)~~
 /// 電流が流れている時にONとしたいので、PullUp から PullDown に変更した
-type ButtonPin = gpio::Pin<gpio::bank0::Gpio19, gpio::FunctionSioInput, gpio::PullDown>;
-// type Button1Pin = gpio::Pin<gpio::bank0::Gpio18, gpio::FunctionSioInput, gpio::PullDown>;
-// type Button2Pin = gpio::Pin<gpio::bank0::Gpio17, gpio::FunctionSioInput, gpio::PullDown>;
-// type Button3Pin = gpio::Pin<gpio::bank0::Gpio16, gpio::FunctionSioInput, gpio::PullDown>;
+type Button0Pin = gpio::Pin<gpio::bank0::Gpio19, gpio::FunctionSioInput, gpio::PullDown>;
+type Button1Pin = gpio::Pin<gpio::bank0::Gpio18, gpio::FunctionSioInput, gpio::PullDown>;
+type Button2Pin = gpio::Pin<gpio::bank0::Gpio17, gpio::FunctionSioInput, gpio::PullDown>;
+type Button3Pin = gpio::Pin<gpio::bank0::Gpio16, gpio::FunctionSioInput, gpio::PullDown>;
 
 /// Since we're always accessing these pins together we'll store them in a tuple.
 /// Giving this tuple a type alias means we won't need to use () when putting them
 /// inside an Option. That will be easier to read.
-type LedAndButtonAndAlarm = (LedPin, ButtonPin, Alarm0);
+struct KeyInterruptComponent {
+    button0: Button0Pin,
+    button1: Button1Pin,
+    button2: Button2Pin,
+    button3: Button3Pin,
+    alarm: Alarm0,
+    button0_has_edge_high: bool,
+    button1_has_edge_high: bool,
+    button2_has_edge_high: bool,
+    button3_has_edge_high: bool,
+}
 
 /// This how we transfer our Led and Button pins into the Interrupt Handler.
 /// We'll have the option hold both using the LedAndButton type.
 /// This will make it a bit easier to unpack them later.
-static GLOBALS: Mutex<RefCell<Option<LedAndButtonAndAlarm>>> = Mutex::new(RefCell::new(None));
+static GLOBAL_KEY_INTERRUPT_COMPONENT: Mutex<RefCell<Option<KeyInterruptComponent>>> =
+    Mutex::new(RefCell::new(None));
 
-static GLOBAL_BUTTON_PRESSED: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
+enum Buttons {
+    Button0,
+    Button1,
+    Button2,
+    Button3,
+}
+
+static GLOBAL_BUTTON_PRESSED_QUEUE: Mutex<RefCell<TODO>> = Mutex::new(RefCell::new(false));
 
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
@@ -111,20 +129,35 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let led = pins.led.reconfigure(); // into_push_pull_output
+    let mut led = pins.led.into_push_pull_output();
+    led.set_high().unwrap();
 
     // Set up the GPIO pin that will be our input
-    let in_pin = pins.gpio19.reconfigure();
-
-    // Trigger on the 'falling edge' of the input pin.
-    // This will happen as the button is being pressed
-    // EdgeLow から EdgeHigh に変更した
-    in_pin.set_interrupt_enabled(EdgeHigh, true);
+    let button0 = pins.gpio19.reconfigure();
+    let button1 = pins.gpio18.reconfigure();
+    let button2 = pins.gpio17.reconfigure();
+    let button3 = pins.gpio16.reconfigure();
+    button0.set_interrupt_enabled(EdgeHigh, true);
+    button1.set_interrupt_enabled(EdgeHigh, true);
+    button2.set_interrupt_enabled(EdgeHigh, true);
+    button3.set_interrupt_enabled(EdgeHigh, true);
 
     // Give away our pins by moving them into the `GLOBAL_PINS` variable.
     // We won't need to access them in the main thread again
     critical_section::with(|cs| {
-        GLOBALS.borrow(cs).replace(Some((led, in_pin, alarm)));
+        GLOBAL_KEY_INTERRUPT_COMPONENT
+            .borrow(cs)
+            .replace(Some(KeyInterruptComponent {
+                button0,
+                button1,
+                button2,
+                button3,
+                alarm,
+                button0_has_edge_high: false,
+                button1_has_edge_high: false,
+                button2_has_edge_high: false,
+                button3_has_edge_high: false,
+            }))
     });
 
     // Configure two pins as being I²C, not GPIO
@@ -186,21 +219,29 @@ fn IO_IRQ_BANK0() {
     debug!("IO_IRQ_BANK0");
 
     critical_section::with(|cs| {
-        let mut binding = GLOBALS.borrow_ref_mut(cs);
-        let (_, ref mut button, ref mut alarm) = binding.as_mut().unwrap();
+        let mut binding = GLOBAL_KEY_INTERRUPT_COMPONENT.borrow_ref_mut(cs);
+        let component = binding.as_mut().unwrap();
         // Check if the interrupt source is from the push button going from high-to-low.
         // Note: this will always be true in this example, as that is the only enabled GPIO interrupt source
-        if button.interrupt_status(EdgeHigh) {
-            debug!("EdgeHigh");
-            // toggle can't fail, but the embedded-hal traits always allow for it
-            // we can discard the return value by assigning it to an unnamed variable
-            // let _ = led.toggle();
+        if component.button0.interrupt_status(EdgeHigh) {
+            debug!("button0 EdgeHigh");
+            component.button0.clear_interrupt(EdgeHigh);
 
-            // Our interrupt doesn't clear itself.
-            // Do that now so we don't immediately jump back to this interrupt handler.
-            button.clear_interrupt(EdgeHigh);
+            // すでにスケジュールが動いている時は上書きしない
+            component.button0_has_edge_high = true;
+            if component.alarm.finished() {
+                component.alarm.schedule(10.millis()).unwrap();
+            }
+        }
+        if component.button1.interrupt_status(EdgeHigh) {
+            debug!("button1 EdgeHigh");
+            component.button1.clear_interrupt(EdgeHigh);
 
-            alarm.schedule(10.millis()).unwrap();
+            // すでにスケジュールが動いている時は上書きしない
+            component.button1_has_edge_high = true;
+            if component.alarm.finished() {
+                component.alarm.schedule(10.millis()).unwrap();
+            }
         }
     })
 }
@@ -210,17 +251,26 @@ fn TIMER_IRQ_0() {
     debug!("TIMER_IRQ_0");
 
     critical_section::with(|cs| {
-        let mut binding = GLOBALS.borrow_ref_mut(cs);
+        let mut binding = GLOBAL_KEY_INTERRUPT_COMPONENT.borrow_ref_mut(cs);
         let button_pressed = GLOBAL_BUTTON_PRESSED.borrow(cs);
-        let (ref mut led, ref mut button, ref mut alarm) = binding.as_mut().unwrap();
+        let component = binding.as_mut().unwrap();
 
-        if button.is_high().unwrap() {
-            info!("BUTTON PRESSED!");
-            button_pressed.replace(true);
-
-            led.toggle().unwrap();
+        if component.button0_has_edge_high {
+            if component.button0.is_high().unwrap() {
+                info!("BUTTON0 PRESSED!");
+                button_pressed.replace(true);
+            }
+            component.button0_has_edge_high = false;
         }
-        alarm.clear_interrupt()
+        if component.button1_has_edge_high {
+            if component.button1.is_high().unwrap() {
+                info!("BUTTON1 PRESSED!");
+                button_pressed.replace(true);
+            }
+            component.button1_has_edge_high = false;
+        }
+
+        component.alarm.clear_interrupt()
     })
 }
 
